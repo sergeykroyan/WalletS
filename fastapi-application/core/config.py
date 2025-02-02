45135439ad2241abd5f949e14c82ba7a6a81c7ba
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-02 14:08:47 +0400

    create db config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
@@ -11,9 +12,18 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = True,
+    echo_pool: bool = True,
+    pool_size: int = 50,
+    max_overflow: int = 10,
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
