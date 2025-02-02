652800b1cf225d7bb185b529715c4134b1b59178
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-02 15:51:29 +0400

    config read from env

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,14 +1,24 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from core.config import settings
 from api import router as api_router
+from core.config import settings
+from core.models import db_helper
 
-app = FastAPI()
-app.include_router(api_router, prefix=settings.api.prefix)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup
+    yield
+    # shutdown
+    await db_helper.dispose()
+
+main_app = FastAPI(lifespan=lifespan)
+main_app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "main:main_app",
         reload=True,
         host=settings.run.host,
         port=settings.run.port,
