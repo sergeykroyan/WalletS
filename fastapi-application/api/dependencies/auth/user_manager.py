e24816032f4234b7f29a2df7e3d053f40945af4c
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 02:21:58 +0400

    create user manager dependence

---
+++ fastapi-application/api/dependencies/auth/user_manager.py
@@ -0,0 +1,7 @@
+from fastapi import Depends
+from .users import get_user_db
+from core.auth.user_manager import UserManager
+
+
+async def get_user_manager(user_db=Depends(get_user_db)):
+    yield UserManager(user_db)
