e6098092e0561505391a8e71569437c4c251cd66
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 02:18:28 +0400

    configs secrets update

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -13,6 +13,8 @@ POSTGRES_INDEXES_NAMING_CONVENTION = {
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
 
 
 class RunConfig(BaseModel):
