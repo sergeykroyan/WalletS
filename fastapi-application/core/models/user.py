2d10277df3762cbaa2f5a5ced0e3a7dc8591fb96
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 00:46:00 +0400

    Add method for user db

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,8 +1,13 @@
-from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import Column, Boolean
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     is_admin = Column(Boolean, default=False)
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, cls)
