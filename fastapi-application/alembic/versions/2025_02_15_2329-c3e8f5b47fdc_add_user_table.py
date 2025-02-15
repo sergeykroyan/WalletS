2aa6ed0310b315baa7ac2186ca93d7785eee7528
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-15 23:35:45 +0400

    Add migration for User model

---
+++ fastapi-application/alembic/versions/2025_02_15_2329-c3e8f5b47fdc_add_user_table.py
@@ -0,0 +1,39 @@
+"""Add User table
+
+Revision ID: c3e8f5b47fdc
+Revises:
+Create Date: 2025-02-15 23:29:07.802031
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "c3e8f5b47fdc"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("is_admin", sa.Boolean(), nullable=True),
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=320), nullable=False),
+        sa.Column("hashed_password", sa.String(length=1024), nullable=False),
+        sa.Column("is_active", sa.Boolean(), nullable=False),
+        sa.Column("is_superuser", sa.Boolean(), nullable=False),
+        sa.Column("is_verified", sa.Boolean(), nullable=False),
+        sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
