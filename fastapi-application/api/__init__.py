4a6ce569f1a05c07a894acc2dcd73a74d9030ca3
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 01:49:00 +0400

    create get database strategy dependence

--- fastapi-application/api/__init__.py
+++ fastapi-application/api/__init__.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+from core.config import settings
+
+# from .api_v1 import router as api_v1_router
+
+router = APIRouter(prefix=settings.api.prefix)
+# router.include_router(api_v1_router)
---
+++ fastapi-application/api/dependencies/auth/strategy.py
@@ -0,0 +1,22 @@
+from core.config import settings
+
+from fastapi import Depends
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
+
+from .access_tokens import get_access_token_db
+from typing import TYPE_CHECKING
+
+
+
+if TYPE_CHECKING:
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
+    from core.models import AccessToken
+
+
+def get_database_strategy(
+    access_tokens_db: "AccessTokenDatabase[AccessToken]" = Depends(get_access_token_db),
+) -> DatabaseStrategy:
+    return DatabaseStrategy(
+        database=access_tokens_db,
+        lifetime_seconds=settings.access_token.lifetime_seconds
+    )
