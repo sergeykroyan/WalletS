67dfb2df3f46e5d3ad31e1547c53bc9c03465582
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 00:51:09 +0400

    fix user model imports

--- fastapi-application/core/models/user.py
+++ fastapi-application/core/models/user.py
@@ -1,13 +1,17 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from sqlalchemy import Column, Boolean
-from sqlalchemy.ext.asyncio import AsyncSession
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     is_admin = Column(Boolean, default=False)
 
     @classmethod
-    def get_db(cls, session: AsyncSession):
+    def get_db(cls, session: "AsyncSession"):
         return SQLAlchemyUserDatabase(session, cls)
