96c109c46bbf393334be1cbfff039080205243a7
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 13:52:58 +0400

    move fastapi users helper to v1

--- fastapi-application/api/v1/fastapi_users.py
+++ fastapi-application/api/v1/fastapi_users.py
@@ -1,7 +1,7 @@
 from fastapi_users import FastAPIUsers
 
 from api.dependencies.auth.user_manager import get_user_manager
-from .backend import auth_backend
+from api.dependencies.auth.backend import auth_backend
 from core.models import User
 
 
