c05c23768660aa7eaaeb192d770a2df3bba2db71
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 13:39:28 +0400

    add user schemas

---
+++ fastapi-application/api/schemas/users.py
@@ -0,0 +1,15 @@
+from fastapi_users import schemas
+from pydantic import Field
+from typing import Optional
+
+
+class UserRead(schemas.BaseUser[int]):
+    is_admin: bool
+
+
+class UserCreate(schemas.BaseUserCreate):
+    is_admin: bool = Field(default=False)
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    is_admin: Optional[bool] = None
