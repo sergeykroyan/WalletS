c5fcb020e1b1582cbd263272ab78612e5f3e3d6c
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-16 20:04:00 +0400

    fix import issue

--- fastapi-application/api/dependencies/auth/access_tokens.py
+++ fastapi-application/api/dependencies/auth/access_tokens.py
@@ -1,13 +1,13 @@
 from typing import TYPE_CHECKING
 from fastapi import Depends
 from core.models import AccessToken
-from core.models.db_helper import DBHelper
+from core.models.db_helper import db_helper
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
 async def get_access_token_db(
-    session: "AsyncSession" = Depends(DBHelper.session_getter),
+    session: "AsyncSession" = Depends(db_helper.session_getter),
 ):
     yield AccessToken.get_db(session=session)
--- fastapi-application/api/dependencies/auth/users.py
+++ fastapi-application/api/dependencies/auth/users.py
@@ -1,11 +1,11 @@
 from typing import TYPE_CHECKING
 from fastapi import Depends
 from core.models import User
-from core.models.db_helper import DBHelper
+from core.models.db_helper import db_helper
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-async def get_user_db(session: "AsyncSession" = Depends(DBHelper.session_getter)):
+async def get_user_db(session: "AsyncSession" = Depends(db_helper.session_getter)):
     yield User.get_db(session=session)
