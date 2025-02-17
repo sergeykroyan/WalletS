7304f3a3b3a59e8dcd8a4953328b1b4bbad8ad24
sergeykroyan <Sergey.Kroyan@volo.global>
2025-02-17 18:24:54 +0400

    make account name unique

---
+++ fastapi-application/alembic/versions/2025_02_17_1824-4ef56832e611_make_account_name_unique.py
@@ -0,0 +1,31 @@
+"""make account name unique
+
+Revision ID: 4ef56832e611
+Revises: 98ddf1d3c802
+Create Date: 2025-02-17 18:24:10.637174
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "4ef56832e611"
+down_revision: Union[str, None] = "98ddf1d3c802"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_unique_constraint(
+        op.f("uq_accounts_account_name"), "accounts", ["account_name"]
+    )
+
+
+def downgrade() -> None:
+    op.drop_constraint(
+        op.f("uq_accounts_account_name"), "accounts", type_="unique"
+    )
